07ecdba6a0551695dbe7a5b442b766f4ac143ea8
wangzhumo <wangzhumoo@gmail.com>
2019-11-07 19:13:00 +0800

    Site updated:2019-11-07 19:12:59

--- js/main.js
+++ js/main.js
@@ -1,126 +1,74 @@
-(function($){
-    // Caption
-    $('.article-entry').each(function(i) {
-        $(this).find('img').filter(function (element) {
-            return $(this).hasClass('');
-        }).each(function() {
-            // add image caption
-            if (this.alt && !(!!$.prototype.justifiedGallery && $(this).parent('.justified-gallery').length)) {
-                $(this).after('<span class="caption">' + this.alt + '</span>');
-            }
+function scrollToElement(target, offset) {
+  var scroll_offset = $(target).offset();
+  $("body,html").animate({
+    scrollTop: scroll_offset.top + (offset || 0),
+    easing: 'swing'
+  })
+}
 
-            if ($(this).parent().prop("tagName") !== 'A') {
-                $(this).wrap('<a href="' + ($(this).attr("data-imgbig") ? $(this).attr("data-imgbig") : this.src) + '" title="' + this.alt + '" class="gallery-item"></a>');
-            }
-        });
-    });
-    if (typeof lightGallery != 'undefined') {
-        var options = {
-            selector: '.gallery-item'
-        };
-        $('.article-entry').each(function(i, entry) {
-            lightGallery(entry, options);
-        });
-        lightGallery($('.article-gallery')[0], options);
+function scrollToBoard() {
+  scrollToElement('#board', -$("#navbar").height());
+}
+
+$(document).ready(function () {
+  // 顶部菜单的动效
+  var navbar = $("#navbar");
+  if (navbar.offset().top > 0) {
+    navbar.addClass("navbar-custom");
+    navbar.removeClass("navbar-dark");
+  }
+  $(window).scroll(function () {
+    if (navbar.offset().top > 0) {
+      navbar.addClass("navbar-custom");
+      navbar.removeClass("navbar-dark");
+    } else {
+      navbar.addClass("navbar-dark");
     }
-    if (!!$.prototype.justifiedGallery) {  // if justifiedGallery method is defined
-        var options = {
-            rowHeight: 140,
-            margins: 4,
-            lastRow: 'justify'
-        };
-        $('.justified-gallery').justifiedGallery(options);
-    }
+  });
+  $('#navbar-toggler-btn').on('click', function () {
+    $('.animated-icon').toggleClass('open');
+    $('#navbar').toggleClass('navbar-col-show');
+  });
 
-    // Profile card
-    var profileElem = $('#profile');
-    $(document).on('click', function () {
-        profileElem.removeClass('card');
-    }).on('click', '#profile-anchor', function (e) {
-        e.stopPropagation();
-        profileElem.toggleClass('card');
-    }).on('click', '.profile-inner', function (e) {
-        e.stopPropagation();
+  // 向下滚动箭头的点击
+  $(".scroll-down-bar").on("click", scrollToBoard);
+
+  // 向顶部滚动箭头
+  var topArrow = $("#scroll-top-button");
+  var posDisplay = false;
+  var scrollDisplay = false;
+  // 位置
+  var setTopArrowPos = function () {
+    var boardRight = document.getElementById('board').getClientRects()[0].right;
+    var bodyWidth = document.body.offsetWidth;
+    var right = bodyWidth - boardRight;
+    posDisplay = right >= 50;
+    topArrow.css({
+      "bottom": posDisplay && scrollDisplay ? "20px" : "-60px",
+      "right": right - 64 + "px"
     });
+  };
+  setTopArrowPos();
+  $(window).resize(setTopArrowPos);
+  // 显示
+  var headerHeight = $("#board").offset().top;
+  $(window).scroll(function () {
+    var scrollHeight = document.body.scrollTop + document.documentElement.scrollTop;
+    scrollDisplay = scrollHeight >= headerHeight;
+    topArrow.css({
+      "bottom": posDisplay && scrollDisplay ? "20px" : "-60px"
+    });
+  });
+  // 点击
+  topArrow.on("click", function () {
+    $("body,html").animate({
+      scrollTop: 0,
+      easing: 'swing'
+    })
+  });
 
-    // To Top
-    var sidebarElem = $('#sidebar'),
-        toTopElem = $('#toTop');
-
-    (function () {
-        if (!sidebarElem.length) return;
-
-        checkDisplayToTop();
-        $(document).on('scroll', function () {
-            checkDisplayToTop();
-        });
-
-        toTopElem.click(function () {
-            $('body, html').animate({ scrollTop: 0 }, 600);
-        });
-
-        var isToTopDisplayed = false;
-        function checkDisplayToTop() {
-            var toTop = (sidebarElem.height() - $(window).height()) + 60;
-            var currentScrollTop = $(document).scrollTop();
-            var needDisplay = (currentScrollTop > toTop) && (currentScrollTop > 0);
-
-            if ($(document).width() >= 800) {
-                if (needDisplay) {
-                    if (isToTopDisplayed) return;
-                    toTopElem.fadeIn();
-                    toTopElem.css('left', sidebarElem.offset().left);
-                    isToTopDisplayed = true;
-                } else {
-                    if (!isToTopDisplayed) return;
-                    toTopElem.fadeOut();
-                    isToTopDisplayed = false;
-                }
-            } else {
-                toTopElem.show();
-                toTopElem.css('right', 20);
-            }
-        }
-    })();
-
-    // Fixed Profile
-    (function () {
-        checkFixedProfile();
-        $(document).on('scroll', function () {
-            checkFixedProfile();
-        });
-
-        var isFixedProfile = false;
-        function checkFixedProfile() {
-            if (!profileElem.is('.profile-fixed')) return;
-            if ($(document).width() < 800) return;
-
-            var currentScrollTop = $(document).scrollTop();
-            var profileInnerElem = $('#profile .profile-inner');
-            var needFixed = currentScrollTop >= profileElem.offset().top + profileElem.outerHeight(true);
-
-            if (needFixed) {
-                if (isFixedProfile) return;
-
-                profileInnerElem.css('position', 'fixed')
-                    .css('width', profileElem.innerWidth() + 'px')
-                    .css('top', '0');
-
-                // css animation fade-in
-                profileInnerElem.css('animation', '');
-                profileInnerElem.addClass('anim-fade-in');
-                isFixedProfile = true;
-            } else {
-                if (!isFixedProfile) return;
-
-                profileInnerElem.css('position', '')
-                    .css('width', '')
-                    .css('top', '');
-
-                profileInnerElem.css('animation', 'none');
-                isFixedProfile = false;
-            }
-        }
-    })();
-
-})(jQuery);
+  // 因兼容问题，在 iOS 和 Safari 环境下不使用固定 Banner
+  if (/(iPhone|iPad|iPod|iOS)/i.test(navigator.userAgent) || (/Safari/i.test(navigator.userAgent) && !/Chrome/i.test(navigator.userAgent))) {
+    $("#background").css("background-attachment", "scroll");
+  }
+});
